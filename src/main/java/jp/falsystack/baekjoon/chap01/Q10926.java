fbdfe6a9a814a7e2e05dc8a48b2ff474b8d7ebcf
falsystack <falsystack@gmail.com>
2023-02-15 00:31:13 +0900

    feat(baekjoon): Q10926

---
+++ src/main/java/jp/falsystack/baekjoon/chap01/Q10926.java
@@ -0,0 +1,19 @@
+package jp.falsystack.baekjoon.chap01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 사이트에 이미 존재하는 아이디가 주어졌을 때, 놀람을 표현하는 프로그램을 작성
+ * joonas -> joonas??!
+ */
+public class Q10926 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String result = br.readLine() + "??!";
+    System.out.println(result);
+  }
+
+}

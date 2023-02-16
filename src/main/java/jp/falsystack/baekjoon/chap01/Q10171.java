4d232b7338d4a2942dbfb88ccd5bbcd9f96455de
falsystack <falsystack@gmail.com>
2023-02-17 00:30:01 +0900

    feat(baekjoon): Q10171

---
+++ src/main/java/jp/falsystack/baekjoon/chap01/Q10171.java
@@ -0,0 +1,14 @@
+package jp.falsystack.baekjoon.chap01;
+
+public class Q10171 {
+
+  public static void main(String[] args) {
+    StringBuilder cat = new StringBuilder();
+    cat.append("\\    /\\\n");
+    cat.append(" )  ( ')\n");
+    cat.append("(  /  )\n");
+    cat.append(" \\(__)|");
+    System.out.println(cat);
+  }
+
+}

6279a4052142a5c38492c4e9678442778d7022a5
falsystack <falsystack@gmail.com>
2023-02-17 23:52:56 +0900

    feat(baekjoon): Q10172

---
+++ src/main/java/jp/falsystack/baekjoon/chap01/Q10172.java
@@ -0,0 +1,16 @@
+package jp.falsystack.baekjoon.chap01;
+
+public class Q10172 {
+
+  public static void main(String[] args) {
+    StringBuilder sb = new StringBuilder();
+    sb.append("|\\_/|\n"
+        + "|q p|   /}\n"
+        + "( 0 )\"\"\"\\\n"
+        + "|\"^\"`    |\n"
+        + "||_/=\\\\__|");
+
+    System.out.println(sb);
+  }
+
+}

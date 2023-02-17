b4174dfeaf265255f2a6bd020413ffc94d568775
falsystack <falsystack@gmail.com>
2023-02-18 00:02:33 +0900

    feat(baekjoon): Q1330

---
+++ src/main/java/jp/falsystack/baekjoon/chap01/Q1330.java
@@ -0,0 +1,25 @@
+package jp.falsystack.baekjoon.chap01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Q1330 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int[] numbers = Arrays
+        .stream(br.readLine()
+            .split(" "))
+            .mapToInt(Integer::parseInt).toArray();
+    if (numbers[0] > numbers[1]) {
+      System.out.println(">");
+    } else if (numbers[0] == numbers[1]) {
+      System.out.println("==");
+    } else {
+      System.out.println("<");
+    }
+  }
+
+}

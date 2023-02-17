3743a03c253baff2a9103eb3fb2bb8fefa370f60
falsystack <falsystack@gmail.com>
2023-02-17 23:55:14 +0900

    feat(baekjoon): Q25083

---
+++ src/main/java/jp/falsystack/baekjoon/chap01/Q25083.java
@@ -0,0 +1,17 @@
+package jp.falsystack.baekjoon.chap01;
+
+public class Q25083 {
+
+  public static void main(String[] args) {
+    StringBuilder sb = new StringBuilder();
+    sb.append("         ,r'\"7\n"
+        + "r`-_   ,'  ,/\n"
+        + " \\. \". L_r'\n"
+        + "   `~\\/\n"
+        + "      |\n"
+        + "      |");
+
+    System.out.println(sb);
+  }
+
+}

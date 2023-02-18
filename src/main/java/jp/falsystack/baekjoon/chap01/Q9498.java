07aa67318b616b68df34ab644d707cb0f2033d77
falsystack <falsystack@gmail.com>
2023-02-18 22:55:04 +0900

    feat(baekjoon): Q9498

---
+++ src/main/java/jp/falsystack/baekjoon/chap01/Q9498.java
@@ -0,0 +1,27 @@
+package jp.falsystack.baekjoon.chap01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 90~100 -> A 80~89 -> B 70~79 -> C 60~69 -> D else F
+ */
+public class Q9498 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int number = Integer.parseInt(br.readLine());
+    if (number >= 90) {
+      System.out.println("A");
+    } else if (number >= 80) {
+      System.out.println("B");
+    } else if (number >= 70) {
+      System.out.println("C");
+    } else if (number >= 60) {
+      System.out.println("D");
+    } else {
+      System.out.println("F");
+    }
+  }
+}

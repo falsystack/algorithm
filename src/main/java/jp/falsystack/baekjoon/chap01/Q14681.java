ceb04d078886c657b581454dc576c2a9925f3e73
falsystack <falsystack@gmail.com>
2023-02-19 19:02:11 +0900

    refactpr(baekjoon): Q14681

--- src/main/java/jp/falsystack/baekjoon/chap01/Q14681.java
+++ src/main/java/jp/falsystack/baekjoon/chap01/Q14681.java
@@ -14,15 +14,6 @@ public class Q14681 {
     int x = Integer.parseInt(br.readLine());
     int y = Integer.parseInt(br.readLine());
 
-//    if (x > 0 && y > 0) {
-//      System.out.println(1);
-//    } else if (x > 0  && y < 0) {
-//      System.out.println("4");
-//    } else if (x < 0 && y > 0) {
-//      System.out.println(2);
-//    } else {
-//      System.out.println(3);
-//    }
     if (x > 0) {
       if (y > 0) {
         System.out.println(1);

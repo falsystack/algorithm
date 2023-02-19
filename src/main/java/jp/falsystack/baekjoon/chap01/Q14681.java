3fa4046dd268ed1e9b67d95939939425b1e0532d
falsystack <falsystack@gmail.com>
2023-02-19 19:00:26 +0900

    feat(baekjoon): Q14681

---
+++ src/main/java/jp/falsystack/baekjoon/chap01/Q14681.java
@@ -0,0 +1,42 @@
+package jp.falsystack.baekjoon.chap01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Quadrant
+ */
+public class Q14681 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int x = Integer.parseInt(br.readLine());
+    int y = Integer.parseInt(br.readLine());
+
+//    if (x > 0 && y > 0) {
+//      System.out.println(1);
+//    } else if (x > 0  && y < 0) {
+//      System.out.println("4");
+//    } else if (x < 0 && y > 0) {
+//      System.out.println(2);
+//    } else {
+//      System.out.println(3);
+//    }
+    if (x > 0) {
+      if (y > 0) {
+        System.out.println(1);
+      } else {
+        System.out.println(4);
+      }
+    } else {
+      if (y > 0) {
+        System.out.println(2);
+      } else {
+        System.out.println(3);
+      }
+    }
+
+  }
+
+}

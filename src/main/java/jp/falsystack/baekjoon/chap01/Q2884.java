2c6ca4eabe861ab26842d15bd2d7bd595ec08046
falsystack <falsystack@gmail.com>
2023-02-20 00:46:14 +0900

    feat(baekjoon): Q2884

---
+++ src/main/java/jp/falsystack/baekjoon/chap01/Q2884.java
@@ -0,0 +1,27 @@
+package jp.falsystack.baekjoon.chap01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Q2884 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String[] times = br.readLine().split(" ");
+
+    int convertedHour = Integer.parseInt(times[0]);
+    int convertedMin = Integer.parseInt(times[1]);
+
+    int hour = convertedHour == 0 ? 24 * 60 : convertedHour * 60;
+
+    int total = ((hour + convertedMin) - 45);
+
+    int alarmHour = total / 60 == 24 ? 0 : total / 60;
+    int alarmMin = total % 60;
+    System.out.print(alarmHour + " " + alarmMin);
+
+  }
+
+}

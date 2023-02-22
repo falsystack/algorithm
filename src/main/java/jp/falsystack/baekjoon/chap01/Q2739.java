ac997c9e1b08c90d4cebfe5521a0293904403ede
falsystack <falsystack@gmail.com>
2023-02-22 21:58:49 +0900

    feat(baekjoon): Q2739

---
+++ src/main/java/jp/falsystack/baekjoon/chap01/Q2739.java
@@ -0,0 +1,21 @@
+package jp.falsystack.baekjoon.chap01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * N을 입력받은 뒤, 구구단 N단을 출력하는 프로그램을 작성하시오. 출력 형식에 맞춰서 출력하면 된다.
+ */
+public class Q2739 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int number = Integer.parseInt(br.readLine());
+
+    for (int i = 1; i <= 9; i++) {
+      System.out.printf("%d * %d = %d\n", number, i, number * i);
+    }
+  }
+
+}

7423a97547b30a6474b4dea576fec474b598095b
falsystack <falsystack@gmail.com>
2023-02-23 22:15:35 +0900

    feat(baekjoon): Q25304

---
+++ src/main/java/jp/falsystack/baekjoon/chap01/Q25304.java
@@ -0,0 +1,27 @@
+package jp.falsystack.baekjoon.chap01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q25304 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    long receiptTotalAmount = Long.parseLong(br.readLine());
+    long totalItems = Long.parseLong(br.readLine());
+    long recalculateTotalAmount = 0;
+    for (int i = 0; i < totalItems; i++) {
+      String[] itemDetails = br.readLine().split(" ");
+      long itemPrice = Long.parseLong(itemDetails[0]);
+      long itemCount = Long.parseLong(itemDetails[1]);
+      recalculateTotalAmount += itemPrice * itemCount;
+    }
+    if (receiptTotalAmount == recalculateTotalAmount) {
+      System.out.println("Yes");
+    } else {
+      System.out.println("No");
+    }
+  }
+
+}

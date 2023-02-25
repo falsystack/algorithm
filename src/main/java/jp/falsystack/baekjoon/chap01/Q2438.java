ed2d88e4f5a30906ad369035093cdc487db7c57d
falsystack <falsystack@gmail.com>
2023-02-26 01:14:44 +0900

    feat(baekjoon): Q2438

---
+++ src/main/java/jp/falsystack/baekjoon/chap01/Q2438.java
@@ -0,0 +1,25 @@
+package jp.falsystack.baekjoon.chap01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * *
+ * **
+ * ***
+ * ****
+ * *****
+ */
+public class Q2438 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int count = Integer.parseInt(br.readLine());
+
+    for (int i = 1; i <= count; i++) {
+      System.out.println("*".repeat(i));
+    }
+  }
+
+}

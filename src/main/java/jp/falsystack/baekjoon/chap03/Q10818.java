b9bded344ccc8d5055184645c791f8e1ffad0a5f
falsystack <falsystack@gmail.com>
2023-03-03 00:02:36 +0900

    feat(baekjoon): Q10818

---
+++ src/main/java/jp/falsystack/baekjoon/chap03/Q10818.java
@@ -0,0 +1,21 @@
+package jp.falsystack.baekjoon.chap03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/**
+ * N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
+ */
+public class Q10818 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    br.readLine();
+    int[] numbers = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+    int max = Arrays.stream(numbers).max().orElseThrow(() -> new RuntimeException("no number"));
+    int min = Arrays.stream(numbers).min().orElseThrow(() -> new RuntimeException("no number"));
+    System.out.println(min + " " + max);
+  }
+}

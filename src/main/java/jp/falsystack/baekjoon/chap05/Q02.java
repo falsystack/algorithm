d57464ce887be36a367c8a9b7e7f90d10db6c957
falsystack <falsystack@gmail.com>
2023-03-09 21:54:21 +0900

    feat(baekjoon): Q02

---
+++ src/main/java/jp/falsystack/baekjoon/chap05/Q02.java
@@ -0,0 +1,17 @@
+package jp.falsystack.baekjoon.chap05;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q02 {
+
+  public static void main(String[] args) {
+    try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+      System.out.println(br.readLine().length());
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+}
